207b7477f9448604bef37cfbad8507c8aca0354a
Jon Haddad <jon@jonhaddad.com>
2016-09-18 12:39:19 -0700

    can add types to the schema, maybe

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -21,7 +21,7 @@ pub struct TypeDef {
 pub struct Schema {
     num_fields: u8,
     // maps a name to an id
-    fields: HashMap<String, u8>,
+    fields: HashMap<String, TypeDef>,
 }
 
 impl Schema {
@@ -29,8 +29,21 @@ impl Schema {
         Schema{num_fields: 0, fields: HashMap::new()}
     }
 
-    pub fn add_type(&mut self, name: &str, dbtype: Type)  {
+    pub fn add_type(&mut self, name: &str, dbtype: Type) {
+        let next_id = self.num_fields + 1;
+        let typedef = TypeDef{ id: next_id, dbtype: dbtype};
+        self.fields.insert(name.to_string(), typedef);
+        self.num_fields += 1;
 
     }
 
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add_type_gets_new_id() {
+
+    }
 }
