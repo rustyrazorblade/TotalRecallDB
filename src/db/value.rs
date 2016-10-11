181ae44895d73fd7eefc103a27fa08c778c7e41f
Jon Haddad <jon@jonhaddad.com>
2016-10-10 20:45:11 -0700

    String support

--- src/db/value.rs
+++ src/db/value.rs
@@ -12,6 +12,12 @@ impl From<i64> for Value {
     fn from(val: i64) -> Value {
         let mut buffer = Vec::new();
         buffer.write_i64::<BigEndian>(val).unwrap();
-        Value{data: buffer}
+        Value { data: buffer }
+    }
+}
+
+impl From<String> for Value {
+    fn from(val: String) -> Value {
+        Value{data:val.into_bytes()}
     }
 }
