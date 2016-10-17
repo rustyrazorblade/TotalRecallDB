e0c7115191f6c27fc8b1b6aee446fb01d2ddda99
Jon Haddad <jon@jonhaddad.com>
2016-10-16 18:43:37 -0700

    making parser available to the db module

--- src/db/parser.rs
+++ src/db/parser.rs
@@ -1,10 +1,17 @@
 peg_file! streamql("streamql.rustpeg");
 
+use self::streamql::{statement, ParseError};
+
+fn parse_statement(query: &str) -> Result<Statement, ParseError> {
+    statement(query)
+}
 
 #[derive(Debug)]
 pub enum Statement {
     Insert(InsertStatement),
 }
+
+
 
 
 #[derive(Debug)]
@@ -16,6 +23,17 @@ impl InsertStatement {
     pub fn new(stream: String) -> InsertStatement {
         InsertStatement{stream: stream}
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_statement;
+    #[test]
+    fn test_basic_inserts() {
+        let result = parse_statement("INSERT INTO test set k=1");
+        assert!(result.is_ok());
+    }
+
 }
 
 
