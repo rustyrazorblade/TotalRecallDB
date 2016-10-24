533e0150fbf6694715808f7ce72385021b2c2c45
Jon Haddad <jon@jonhaddad.com>
2016-10-23 23:39:34 -0700

    test repl coming

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,28 @@
 
 #[macro_use]
 extern crate nom;
+extern crate clap;
 
 mod db;
 
 use db::server::Server;
+use clap::{Arg, App, SubCommand};
 
 fn main() {
-    println!("Hello, world!");
+    println!("Starting the worst database ever created!!");
     let server = Server::new();
+    let app = App::new("StreamingDB")
+                  .version("v1.0")
+                  .author("Jon Haddad, <jon@jonhaddad.com>")
+                  .subcommand(SubCommand::with_name("test"))
+                  .get_matches();
+
+    if let Some(matches) = app.subcommand_matches("test") {
+        run_test_repl();
+    }
+
+}
+
+fn run_test_repl() {
+    println!("Running test repl");
 }
