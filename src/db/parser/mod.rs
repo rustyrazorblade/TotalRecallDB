ed2a2eaba98f2ffc18ca83f1cd9639b1c51e4828
Jon Haddad <jon@jonhaddad.com>
2016-10-23 22:15:44 -0700

    getting column spec going

--- src/db/parser/mod.rs
+++ src/db/parser/mod.rs
@@ -22,7 +22,7 @@ pub enum Statement {
     // Stream & Keys
     Insert(String, RowBuilder),
     // name
-    DeclareStream(String),
+    DeclareStream(String, Vec<ColumnSpec>),
     DropStream,
     UseDatabase,
     Select,
@@ -31,7 +31,14 @@ pub enum Statement {
 
 #[derive(Debug)]
 pub struct ColumnSpec {
-    ftype: Type,
+    name: String,
+//    ftype: Type,
+}
+
+impl ColumnSpec {
+    fn new(name: String) -> ColumnSpec {
+        ColumnSpec{name: name}
+    }
 }
 
 #[cfg(test)]
