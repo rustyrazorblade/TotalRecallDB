2635a95a7c3f3d5528f78e88d0afab7cd448ef28
Jon Haddad <jon@jonhaddad.com>
2016-10-29 11:35:44 -0700

    adding row serializer

---
+++ src/db/storage/page.rs
@@ -0,0 +1,32 @@
+use db::row::Row;
+// mis info about what's on each page.
+const HEADER_SIZE_IN_BYTES : usize = 128;
+
+struct Page {
+    header: Header,
+    data: [u8; 4096 - HEADER_SIZE_IN_BYTES],
+    pos: u16,
+}
+
+enum PageError {
+    Full
+}
+
+struct Header;
+
+impl Page {
+    fn new() -> Page {
+        Page{header: Header::new(),
+             data: [0; 4096 - HEADER_SIZE_IN_BYTES],
+             pos: 0}
+    }
+    fn write(&mut self, row: &Row) -> Result<(), PageError> {
+        Err(PageError::Full)
+    }
+}
+
+impl Header {
+    fn new() -> Header {
+        Header{}
+    }
+}
