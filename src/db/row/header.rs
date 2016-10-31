9ee4df89386f88ec48734b17482fcddd52f6ca7b
Jon Haddad <jon@jonhaddad.com>
2016-10-30 17:49:42 -0700

    removed header

--- src/db/row/header.rs
+++ src/db/row/header.rs
@@ -3,34 +3,36 @@ Header for a row
 
 Stores a Vector of field & offset pairs
 Used with a Row to get correct slices of data to create Values
+
+Headers are 4 byte pairs - 2 for field id and 2 for offset
 */
 
-#[derive(Clone, Debug)]
-pub struct Header {
-    offsets: Vec<(u8, u16)>
+enum HeaderError {
+    FieldExists
 }
+
+#[derive(Clone, Debug)]
+pub struct Header;
 
 impl Header {
     pub fn new() -> Header {
-        Header{offsets:Vec::new()}
+        Header{}
     }
-    // returns an offset in number bytes for the byte data in a Row
-    pub fn get_offset(field: u8) -> u16 {
-        0
-    }
-
-    // should this consume self? do I need a header after it's written out?
-    // how will that work with a Row?
-    // I don't think it can..  making it take &self for now and return new bytes
-    pub fn into_vec(&self) -> Vec<u8> {
-        Vec::new()
-    }
-
 
 }
 
 impl<'a> From<&'a [u8]> for Header {
     fn from(bytes: &'a [u8]) -> Header {
         Header::new()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_normal_header() {
+        let h = Header::new();
     }
 }
