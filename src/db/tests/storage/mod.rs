cd3b7e497d6562ceab134f93de0cdb67da3479eb
Jon Haddad <jon@jonhaddad.com>
2017-01-05 20:04:57 -0800

    creating 2 engines

--- src/db/tests/storage/mod.rs
+++ src/db/tests/storage/mod.rs
@@ -4,12 +4,19 @@ pub mod memory;
 #[cfg(test)]
 mod tests {
     use db::storage::{Memory, Disk, StorageEngine};
+    use tempdir::TempDir;
 
     #[test]
-    fn test_storage_interface() {
+    fn test_storage_engine_creation() {
         let mem = Memory::new().expect("Memory storage");
         let storage = StorageEngine::new(mem);
 
+        let tmp = TempDir::new("totalrecallwhatever").expect("Expected dir");
+        let mut tmp2 = tmp.into_path();
+        tmp2.push("blah");
+
+        let disk = Disk::new(10, tmp2).expect("Disk storage");
+        let s2 = StorageEngine::new(disk);
     }
 
 }
