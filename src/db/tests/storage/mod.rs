7cb371f6e774e1c832675d86f2ded1e4e3312acc
Jon Haddad <jon@jonhaddad.com>
2017-01-13 22:00:00 -0800

    cool first cut at a macro for testing storage

--- src/db/tests/storage/mod.rs
+++ src/db/tests/storage/mod.rs
@@ -1,22 +1,34 @@
 pub mod disk;
 pub mod memory;
 
-#[cfg(test)]
-mod tests {
-    use db::storage::{Memory, Disk, StorageEngine};
-    use tempdir::TempDir;
+use db::storage::{Memory, Disk, StorageEngine};
+use tempdir::TempDir;
+use db::storage::Storage;
 
-    #[test]
-    fn test_storage_engine_creation() {
-        let mem = Memory::new().expect("Memory storage");
-        let storage = StorageEngine::new(mem);
+macro_rules! test_storage {
+    ( $f:ident, $x:ident, $y:ident ) => {
+        $f(&$x);
+        $f(&$y);
+    };
+}
 
-        let tmp = TempDir::new("totalrecallwhatever").expect("Expected dir");
-        let mut tmp2 = tmp.into_path();
-        tmp2.push("blah");
+#[test]
+fn test_storage_engine_creation() {
+    let mem = Memory::new().expect("Memory storage");
+    let storage = StorageEngine::new(mem);
 
-        let disk = Disk::new(10, tmp2).expect("Disk storage");
-        let s2 = StorageEngine::new(disk);
-    }
+    // disk storage
+    let tmp = TempDir::new("totalrecallwhatever").expect("Expected dir");
+    let mut tmp2 = tmp.into_path();
+    tmp2.push("blah");
+
+    let disk = Disk::new(10, tmp2).expect("Disk storage");
+    let storage2 = StorageEngine::new(disk);
+
+    test_storage!(test_page_write_and_get, storage, storage2);
+}
+
+fn test_page_write_and_get(s: &StorageEngine) {
 
 }
+
